b2d0425bc587f31915c37fc8efcd46e8e229611c
lakshmirmenon <menon.r.lakshmi@gmail.com>
2024-11-05 12:10:49 +0530

    clubs added

--- src/components/home/components/clubsPage/clubs.jsx
+++ src/components/home/components/clubsPage/clubs.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import "./clubs.css";
 import Navbar from "../../../Navbar";
 
-function Clubs(){
-    return <><Navbar logout={"true"} /></>
+function Clubs() {
+    return (
+        <>
+            <Navbar logout={"true"} />
+            <div className="clubs-container">
+                <h1>     </h1>
+                <div className="clubs-list">
+                    {["Tech Club", "Music Club", "Art Club", "Sports Club"].map((club, index) => (
+                        <div className="club-item" key={index}>
+                            <span>{club}</span>
+                            <button className="request-btn">Request to Join</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Clubs;
